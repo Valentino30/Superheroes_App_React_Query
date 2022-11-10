76c106dfbd7e2bd6a78dcdf77704c67075c79798
Valentino <valentino.giudice@network.elancoah.com>
2022-11-10 10:48:04 +0100

    add: get and display superheroes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import axios from "axios";
+import { useQuery } from "react-query";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Hero } from "./types/hero";
+
+const getSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+const App = () => {
+  const { isLoading, data, error } = useQuery("super-heroes", getSuperHeroes);
+  if (isLoading) return <h2>Loading ...</h2>;
+  if (error) return <h2>Something went wrong</h2>;
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h2>My Superheroes</h2>
+      <ul>
+        {data?.data.map((hero: Hero) => (
+          <li key={hero.id}>
+            <strong>{hero.name}</strong>: {hero.alterEgo}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
