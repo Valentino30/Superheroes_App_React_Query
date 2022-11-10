de0b998a232f9872c5bc52e697c04ea8fb6889c9
Valentino <valentino.giudice@network.elancoah.com>
2022-11-10 10:50:50 +0100

    add: react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
-import App from "./App";
+
 import "./index.css";
+import App from "./App";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
